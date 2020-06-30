af88a3244199ff36ef6630f2621f443cbbdd272f
kspavankrishna <58327514+kspavankrishna@users.noreply.github.com>
2020-06-30 21:49:58 +0530

    SINGLETON

--- MyApp.java
+++ MyApp.java
@@ -1,18 +1,14 @@
-package simpleFactory;
+package Singleton;
 
-import java.util.Scanner;
+public class MyApp{
 
-public class MyApp {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter name");
-		String name = sc.nextLine();
-		NameFactory myFactory = new NameFactory();
-		NameDivider nd = myFactory.getInstance(name);
-		System.out.println("First Name = "+nd.getFirstName());
-		System.out.println("Last Name = "+nd.getLastName());
+		MyThread t = new MyThread();
+		t.start();
+		t.join();
+		Singleton.getInstance();
+		
 
 	}
 
